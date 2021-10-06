c993c0fef51cf5614ed44fc6a6cd6371bc8e41be
Tomas Berhane <68202596+tomas-collab@users.noreply.github.com>
2021-10-06 04:27:57 +0200

    more tests

--- src/__tests__/index.test.js
+++ src/__tests__/index.test.js
@@ -37,5 +37,65 @@ describe("Testing the server",()=>{
         expect(response.body.message).toBe("test success")
     })
 
-    it()
+    it("should test that a /non existent endpoint is returning 404", async () => {
+        const response = await request.get('/not-existing')
+
+        expect(response.status).toBe(404)
+    })
+
+    const validProduct = {
+        name: 'iPhone',
+        price: 900
+    }
+
+    it("should test that a POST /products is returning us a valid product", async () => {
+        const response = await request.post('/products').send(validProduct)
+
+        expect(response.status).toBe(201)
+        expect(response.body._id).toBeDefined()
+
+    })
+
+    const invalidProduct = {
+        price: 900
+    }
+
+    it("should test that a POST /products is returning us an error with an invalid body", async () => {
+        const response = await request.post('/products').send(invalidProduct)
+
+        expect(response.status).toBe(400)
+        expect(response.body._id).not.toBeDefined()
+
+    })
+
+    it("should test that a GET /products endpoint is returning a valid product", async () => {
+        const response = await request.post('/products').send(validProduct)
+
+        expect(response.status).toBe(201)
+        expect(response.body._id).toBeDefined()
+
+        const idResponse = await request.get('/products/' + response.body._id)
+        expect(idResponse.body.name).toEqual(validProduct.name)
+    })
+
+    it("should test that the /products endpoint is returning an updated product", async () => {
+        const response = await request.post('/products').send(validProduct)
+
+        expect(response.status).toBe(201)
+        expect(response.body._id).toBeDefined()
+
+        const idResponse = await request.put('/products/' + response.body._id)
+        expect(idResponse.body.name).toEqual(validProduct.name)
+        expect(typeof idResponse.body.name).toBe('string')
+    })
+
+    it("should test that the /products endpoint is returning a valid response to a deleted product", async () => {
+        const response = await request.post('/products').send(validProduct)
+
+        expect(response.status).toBe(201)
+        expect(response.body._id).toBeDefined()
+
+        const idResponse = await request.delete('/products/' + response.body._id)
+        expect(idResponse.status).toBe(204)
+    })
 })
